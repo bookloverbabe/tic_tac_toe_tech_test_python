e591f376bb87aec53d7e1d18592b434963939f20
bookloverbabe <solmaz@hotmail.co.uk>
2023-05-25 17:34:30 +0100

    Switch to ubuntu

---
+++ lib/board.py
@@ -0,0 +1,7 @@
+# This class creates the grid for the game, consists of three rows of three
+class Board:
+  # The initialise method below creates the board instance variable
+  def __init__(self):
+    # Self is used to create an instance variable in Python
+    self.board = []
+
